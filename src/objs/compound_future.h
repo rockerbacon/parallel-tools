10c67616eb9426face1ceb24ded357011b0750fb
rockerbacon <vitor@librem.one>
2019-12-25 00:26:15 -0300

    Implemented compound future

---
+++ src/objs/compound_future.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <future>
+
+namespace parallel_tools {
+
+	template<typename T>
+	class compound_future {
+		private:
+			std::shared_ptr<T> return_value;
+			std::future<void> future;
+
+		public:
+			compound_future (const decltype(return_value)& return_value, decltype(future)&& future) :
+				return_value(return_value),
+				future(std::move(future))
+			{}
+
+			void wait () {
+				future.wait();
+			}
+
+			T get() {
+				wait();
+				return *return_value;
+			}
+	};
+}
