0523514d3a0cbe72c0248fa1f3670f5f0cab150e
rockerbacon <vas.vitor.santos@gmail.com>
2020-03-15 23:38:00 -0300

    Adjusted benchmark

--- src/main/benchmarks/thread_pool/void_signature.cpp
+++ src/main/benchmarks/thread_pool/void_signature.cpp
@@ -5,16 +5,16 @@
 
 #include <thread_pool.h>
 
-#define MIN_THREADS 2
+#define MIN_THREADS 4
 #define MAX_THREADS 64
-#define TASKS_PER_RUN 500'000
+#define TASKS_PER_RUN 100'000
 #define RUNS 50
 
 #define SETUP_BENCHMARK()\
 	TerminalObserver terminal_observer;\
 	chrono::high_resolution_clock::duration production_time,\
 											consumption_time,\
-											run_time;\
+											delay;\
 	unsigned run;\
 	float progress;\
 \
@@ -22,17 +22,17 @@
 \
 	observe(progress, percentage_complete);\
 \
-	observe_average(production_time, average_production_time);\
-	observe_minimum(production_time, fastest_production_time);\
-	observe_maximum(production_time, slowest_production_time);\
+	observe_average(production_time, avg_production_time);\
+	observe_minimum(production_time, min_production_time);\
+	observe_maximum(production_time, max_production_time);\
 \
-	observe_average(consumption_time, average_consumption_time);\
-	observe_minimum(consumption_time, fastest_consumpion_time);\
-	observe_maximum(consumption_time, slowest_consumption_time);\
+	observe_average(consumption_time, avg_consumption_time);\
+	observe_minimum(consumption_time, min_consumpion_time);\
+	observe_maximum(consumption_time, max_consumption_time);\
 \
-	observe_average(run_time, average_run_time);\
-	observe_minimum(run_time, fastest_run_time);\
-	observe_maximum(run_time, slowest_run_time);\
+	observe_average(delay, avg_delay);\
+	observe_minimum(delay, min_delay);\
+	observe_maximum(delay, max_delay);\
 
 
 using namespace benchmark;
@@ -47,35 +47,30 @@ int main() {
 		string benchmark_description = "parallel_tools::thread_pool with void() method and "s + to_string(threads) + " threads";
 		benchmark(benchmark_description, RUNS) {
 			vector<future<void>> tasks_futures; tasks_futures.reserve(TASKS_PER_RUN);
-			vector<chrono::high_resolution_clock::duration> tasks_consumption_time(TASKS_PER_RUN);
-			vector<chrono::high_resolution_clock::duration> tasks_production_time(TASKS_PER_RUN);
-			vector<stopwatch> consumption_stopwatches(TASKS_PER_RUN);
 
-			stopwatch run_stopwatch;
+			vector<chrono::high_resolution_clock::duration> consumption_times(TASKS_PER_RUN);
+
+			vector<stopwatch> consumption_stopwatches(TASKS_PER_RUN);
+			stopwatch production_stopwatch;
 			for (unsigned i = 0; i < TASKS_PER_RUN; i++) {
 				auto task = [
-					&tasks_consumption_time,
-					i,
-					&consumption_stopwatches
-				] () mutable {
-					tasks_consumption_time[i] = consumption_stopwatches[i].lap_time();
+					&consumption_time = consumption_times[i],
+					&consumption_stopwatch = consumption_stopwatches[i]
+				] () {
+					consumption_time = consumption_stopwatch.lap_time();
 				};
 
-				stopwatch production_stopwatch;
-				consumption_stopwatches[i].reset();
-				auto future = pool.exec(task);
-				consumption_stopwatches[i].reset();
-				tasks_production_time[i] = production_stopwatch.lap_time();
-
-				tasks_futures.emplace_back(std::move(future));
+				tasks_futures.emplace_back(pool.exec(task));
 			}
+			production_time = production_stopwatch.lap_time();
 
 			for (auto& future : tasks_futures) {
 				future.wait();
 			}
-			run_time = run_stopwatch.lap_time();
-			consumption_time = *max_element(tasks_consumption_time.begin(), tasks_consumption_time.end());
-			production_time = *max_element(tasks_production_time.begin(), tasks_production_time.end());
+
+			consumption_time = *max_element(consumption_times.begin(), consumption_times.end());
+
+			delay = consumption_time - production_time;
 
 			run++;
 			progress = (float)run/RUNS*100.0f;
